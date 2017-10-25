28174f138b3a4fdeec45a76bccde06a8fc935c81
Raymon Garcia <mac@Raymons-MacBook-Pro.local>
2017-10-25 18:38:28 -0400

    make it better

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friends = [
+	{
+		name: "Bobby",
+		picture: "http://images.contentful.com/7h71s48744nc/2vGcHOqRAsMe6Q4yoU8qGY/164ff0aa7e9562cf5895131e3ba720da/the-waterboy.jpeg",
+		scores: [
+		
+		]
+	}
+]
